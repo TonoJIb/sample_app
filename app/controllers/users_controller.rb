3917054787d750129943e31a63571d1f5b53a7c2
TonoJIb <mobil1234@inbox.ru>
2012-12-03 16:50:44 +0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
+  before_filter :not_signed_in_user, only: [:new, :create]
   def new
   end
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   def new
     @user = User.new
@@ -44,17 +46,14 @@ class UsersController < ApplicationController
   end
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in." 
-      end
-    end  
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+    def not_signed_in_user
+        redirect_to(root_path) and return while signed_in?
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,6 +13,11 @@ describe "Authentication" do
       it { should have_selector('h1',    text: 'Sign in') }
       it { should have_selector('title', text: 'Sign in') }
       it { should have_error_message('Invalid') }
+      it { should_not have_link('Users',    href: users_path) }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out', href: signout_path) }
+      it { should have_link('Sign in', href: signin_path) }
       
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -51,29 +56,41 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       
+           
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in(user)
         end
-
+         
         describe "after signing in" do
-
+         
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
-        end
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+          
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
+          end          
+        end      
       end
       
       describe "in the Users controller" do
-
+      
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
         end
-
+            
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
@@ -83,6 +100,20 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
       end
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    
+    
     end
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
